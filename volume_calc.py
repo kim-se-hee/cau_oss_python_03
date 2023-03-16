7a45bb27f9604223a356eb32cbc6db317daac148
kim-se-hee <44455488+kim-se-hee@users.noreply.github.com>
2023-03-16 16:07:19 +0900

    Create volume_calc.py
    1주차 과제 코드 작성

---
+++ volume_calc.py
@@ -0,0 +1,8 @@
+width = 1
+width = float(input("가로 : "))
+length = 1
+length = float(input("세로 : "))
+height = 1
+height = float(input("가로 : "))
+volume = width * length* height
+print("부피 : ", volume)
